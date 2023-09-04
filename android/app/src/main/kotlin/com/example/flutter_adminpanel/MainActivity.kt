3e0050682c00d9f4682d592fc1433c01c10b3778
aliasghar5124 <92795509+aliasghar5124@users.noreply.github.com>
2023-09-04 15:44:16 +0500

    first

---
+++ android/app/src/main/kotlin/com/example/flutter_adminpanel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_adminpanel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/RecentFile.dart
@@ -0,0 +1,50 @@
+class RecentFile {
+  final String? icon, title, date, size;
+
+  RecentFile({this.icon, this.title, this.date, this.size});
+}
+
+List demoRecentFiles = [
+  RecentFile(
+    icon: "assets/icons/xd_file.svg",
+    title: "XD File",
+    date: "04-09-2023",
+    size: "3.5mb",
+  ),
+  RecentFile(
+    icon: "assets/icons/Figma_file.svg",
+    title: "Figma File",
+    date: "04-09-2023",
+    size: "19.0mb",
+  ),
+  RecentFile(
+    icon: "assets/icons/doc_file.svg",
+    title: "Document",
+    date: "04-09-2023",
+    size: "32.5mb",
+  ),
+  RecentFile(
+    icon: "assets/icons/sound_file.svg",
+    title: "Sound File",
+    date: "04-09-2023",
+    size: "3.5mb",
+  ),
+  RecentFile(
+    icon: "assets/icons/media_file.svg",
+    title: "Media File",
+    date: "04-09-2023",
+    size: "2.5gb",
+  ),
+  RecentFile(
+    icon: "assets/icons/pdf_file.svg",
+    title: "Sales PDF",
+    date: "04-09-2023",
+    size: "3.5mb",
+  ),
+  RecentFile(
+    icon: "assets/icons/excel_file.svg",
+    title: "Excel File",
+    date: "04-09-2023",
+    size: "34.5mb",
+  ),
+];
